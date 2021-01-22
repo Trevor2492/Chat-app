f8f62134155304b8a47b8a24d54922c3821d5cc2
Trevor <trevor2492@gmail.com>
2021-01-21 20:40:01 -0600

    Used Firebase/firestore to store message data on the server-side and user authentication

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import '@firebase/auth';
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends Component {
 
@@ -8,43 +12,107 @@ export default class Chat extends Component {
 		super();
 		this.state = {
 			messages: [],
+			user: {
+				_id: '',
+				name: '',
+        avatar: '',
+        createdAt: ''
+			}
 		}
+
+
+		// grabs the database from firebase/cloud firestore (initializes the firestore app)
+    if (!firebase.apps.length){
+      firebase.initializeApp({
+				apiKey: "AIzaSyCCG1qp7vArHbojdS_aW_alV-le3JRA68A", // pulled all this stuff from firestore
+				authDomain: "chat-app-41bbf.firebaseapp.com",
+				projectId: "chat-app-41bbf",
+				storageBucket: "chat-app-41bbf.appspot.com",
+				messagingSenderId: "413945673648",
+				appId: "1:413945673648:web:15af400ffc12a88e018875",
+				measurementId: "G-RF8CN0CXF7"
+      });
+    }
 	}
 
 	componentDidMount() {
-		const name = this.props.route.params.name;
+		const name = this.props.route.params.name; // sent over from the start screen
+		this.props.navigation.setOptions({ title: name }); // Sets the title of the chat page to be the name of the user.
 
-		// Sets the title of the chat page to be the name of the user. THIS IS WHERE THE WARNING IS COMING FROM
-		this.props.navigation.setOptions({ title: name }); 
+		this.referenceChatMessages = firebase.firestore().collection("messages");
+		// this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate);
 
+		this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+			if (!user) {
+				firebase.auth().signInAnonymously();
+			}
 
-		this.setState({
-			messages: [
-				{
-					_id: 1,
-					text: 'Hello developer',
-					createdAt: new Date(),
-					user: {
-						_id: 2,
-						name: 'React Native',
-						avatar: 'https://placeimg.com/140/140/any',
-					},
+			this.setState({
+				user: {
+					_id: user.uid,
+					name: name,
+					avatar: "https://placeimg.com/140/140/any",
+					createdAt: new Date()
 				},
-				{
-					_id: 2,
-					text: name + ' has entered the chat',
-					createdAt: new Date(),
-					system: true,
-				},
-			],
-		})
+				messages: [],
+			});
+
+			this.unsubscribe = this.referenceChatMessages
+				.orderBy("createdAt", "desc")
+				.onSnapshot(this.onCollectionUpdate);
+
+		});
+
 	}
 
-	onSend(messages = []){
-		this.setState(previousState => ({
-			messages: GiftedChat.append(previousState.messages, messages),
-		}));
+	componentWillUnmount(){
+		this.unsubscribe();
+		this.authUnsubscribe();
 	}
+
+	onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+			var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        }
+      });
+    });
+    this.setState({
+      messages,
+    });
+  }
+
+	onSend(messages = []) {
+    // setState is called with previousState as the parameter -> reference to the component's
+    // state at the time the change is applied.
+    this.setState(previousState => ({
+      // the append function by Gifted chat appends the new message to the message obj.
+      messages: GiftedChat.append(previousState.messages, messages),
+    }),
+    () => {
+      this.addMessage();
+    });
+  }
+
+	addMessage() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  }
 
 	renderBubble(props) {
 		return (
@@ -52,7 +120,7 @@ export default class Chat extends Component {
 				{...props}
 				wrapperStyle={{
 					right: {
-						backgroundColor: '#000'
+						backgroundColor: 'darkgray'
 					}
 				}}
 			/>
@@ -60,7 +128,7 @@ export default class Chat extends Component {
 	}
 
 	render() {
-		// passed these from the "start" screen
+		// passed this from the "start" screen
 		let color = this.props.route.params.color;
 
 		return (
@@ -70,7 +138,7 @@ export default class Chat extends Component {
 					renderBubble={this.renderBubble.bind(this)}
 					messages={this.state.messages}
 					onSend={messages => this.onSend(messages)}
-					user={{_id: 1}}
+					user={this.state.user}
 				/>
 
 				{ Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height'/> : null }
